db8471e3d4ef2dff0a489769a663d4ba4900b7b4
Victor Wang <wjp2013@gmail.com>
2014-07-31 20:02:36 +0800

    升级 rails 4.1.4 相关的配置文件

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( backend.css )
+Rails.application.config.assets.precompile += %w( backend.js ie/html5shiv.js ie/respond.min.js ie/excanvas.js)
